a049dc875a2643d22126367d4aa551dd3c301c7f
Danbloom3211 <86500737+Danbloom3211@users.noreply.github.com>
2021-06-25 19:30:53 -0400

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,764 @@
+function CopyToLabel()
+			{
+	        	//Reference TextBox.
+	        	var txtName = document.getElementById("txtName");
+	 
+	        	//Reference Label.
+	        	var lblName = document.getElementById("lblName");
+	 
+	        	//Copy TextBox value to Label.
+	        	lblName.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel2()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName2 = document.getElementById("lblName2");
+
+	        	lblName2.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel3()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName3 = document.getElementById("lblName3");
+
+	        	lblName3.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel4()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName4 = document.getElementById("lblName4");
+
+	        	lblName4.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel5()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName5 = document.getElementById("lblName5");
+
+	        	lblName5.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel6()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName6 = document.getElementById("lblName6");
+
+	        	lblName6.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel7()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName7 = document.getElementById("lblName7");
+
+	        	lblName7.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel8()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName8 = document.getElementById("lblName8");
+
+	        	lblName8.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel9()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName9 = document.getElementById("lblName9");
+
+	        	lblName9.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel10()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName10 = document.getElementById("lblName10");
+
+	        	lblName10.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel11()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName11 = document.getElementById("lblName11");
+
+	        	lblName11.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel12()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName12 = document.getElementById("lblName12");
+
+	        	lblName12.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel13()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName13 = document.getElementById("lblName13");
+
+	        	lblName13.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel14()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName14 = document.getElementById("lblName14");
+
+	        	lblName14.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel15()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName15 = document.getElementById("lblName15");
+
+	        	lblName15.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel16()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName16 = document.getElementById("lblName16");
+
+	        	lblName16.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel17()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName17 = document.getElementById("lblName17");
+
+	        	lblName17.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel18()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName18 = document.getElementById("lblName18");
+
+	        	lblName18.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel19()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName19 = document.getElementById("lblName19");
+
+	        	lblName19.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel20()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName20 = document.getElementById("lblName20");
+
+	        	lblName20.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel21()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName21 = document.getElementById("lblName21");
+
+	        	lblName21.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel22()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName22 = document.getElementById("lblName22");
+
+	        	lblName22.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel23()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName23 = document.getElementById("lblName23");
+
+	        	lblName23.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel24()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName24 = document.getElementById("lblName24");
+
+	        	lblName24.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel25()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName25 = document.getElementById("lblName25");
+
+	        	lblName25.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel26()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName26 = document.getElementById("lblName26");
+
+	        	lblName26.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel27()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName27 = document.getElementById("lblName27");
+
+	        	lblName27.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel28()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName28 = document.getElementById("lblName28");
+
+	        	lblName28.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel29()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName29 = document.getElementById("lblName29");
+
+	        	lblName29.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel30()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName30 = document.getElementById("lblName30");
+
+	        	lblName30.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel31()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName31 = document.getElementById("lblName31");
+
+	        	lblName31.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel32()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName32 = document.getElementById("lblName32");
+
+	        	lblName32.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel33()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName33 = document.getElementById("lblName33");
+
+	        	lblName33.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel34()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName34 = document.getElementById("lblName34");
+
+	        	lblName34.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel35()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName35 = document.getElementById("lblName35");
+
+	        	lblName35.innerHTML = txtName.value;
+    		}
+
+    		function CopyToLabel36()
+			{
+	        	var txtName = document.getElementById("txtName");
+	        	var lblName36 = document.getElementById("lblName36");
+
+	        	lblName36.innerHTML = txtName.value;
+    		}
+
+    		function Delete1()
+    		{
+    			document.getElementById("lblName").innerHTML = ' ';
+    		}
+
+    		function Delete2()
+    		{
+    			document.getElementById("lblName2").innerHTML = ' ';
+    		}
+
+    		function Delete3()
+    		{
+    			document.getElementById("lblName3").innerHTML = ' ';
+    		}
+
+    		function Delete4()
+    		{
+    			document.getElementById("lblName4").innerHTML = ' ';
+    		}
+
+    		function Delete5()
+    		{
+    			document.getElementById("lblName5").innerHTML = ' ';
+    		}
+
+    		function Delete6()
+    		{
+    			document.getElementById("lblName6").innerHTML = ' ';
+    		}
+
+    		function Delete7()
+    		{
+    			document.getElementById("lblName7").innerHTML = ' ';
+    		}
+
+    		function Delete8()
+    		{
+    			document.getElementById("lblName8").innerHTML = ' ';
+    		}
+
+    		function Delete9()
+    		{
+    			document.getElementById("lblName9").innerHTML = ' ';
+    		}
+
+    		function Delete10()
+    		{
+    			document.getElementById("lblName10").innerHTML = ' ';
+    		}
+
+    		function Delete11()
+    		{
+    			document.getElementById("lblName11").innerHTML = ' ';
+    		}
+
+    		function Delete12()
+    		{
+    			document.getElementById("lblName12").innerHTML = ' ';
+    		}
+
+    		function Delete13()
+    		{
+    			document.getElementById("lblName3").innerHTML = ' ';
+    		}
+
+    		function Delete14()
+    		{
+    			document.getElementById("lblName14").innerHTML = ' ';
+    		}
+
+    		function Delete15()
+    		{
+    			document.getElementById("lblName15").innerHTML = ' ';
+    		}
+
+    		function Delete16()
+    		{
+    			document.getElementById("lblName16").innerHTML = ' ';
+    		}
+
+    		function Delete17()
+    		{
+    			document.getElementById("lblName17").innerHTML = ' ';
+    		}
+
+    		function Delete18()
+    		{
+    			document.getElementById("lblName18").innerHTML = ' ';
+    		}
+
+    		function Delete19()
+    		{
+    			document.getElementById("lblName19").innerHTML = ' ';
+    		}
+
+    		function Delete20()
+    		{
+    			document.getElementById("lblName20").innerHTML = ' ';
+    		}
+
+    		function Delete21()
+    		{
+    			document.getElementById("lblName21").innerHTML = ' ';
+    		}
+
+    		function Delete22()
+    		{
+    			document.getElementById("lblName22").innerHTML = ' ';
+    		}
+
+    		function Delete23()
+    		{
+    			document.getElementById("lblName23").innerHTML = ' ';
+    		}
+
+    		function Delete24()
+    		{
+    			document.getElementById("lblName24").innerHTML = ' ';
+    		}
+
+    		function Delete25()
+    		{
+    			document.getElementById("lblName25").innerHTML = ' ';
+    		}
+
+    		function Delete26()
+    		{
+    			document.getElementById("lblName26").innerHTML = ' ';
+    		}
+
+    		function Delete27()
+    		{
+    			document.getElementById("lblName27").innerHTML = ' ';
+    		}
+
+    		function Delete28()
+    		{
+    			document.getElementById("lblName28").innerHTML = ' ';
+    		}
+
+    		function Delete29()
+    		{
+    			document.getElementById("lblName29").innerHTML = ' ';
+    		}
+
+    		function Delete30()
+    		{
+    			document.getElementById("lblName30").innerHTML = ' ';
+    		}
+
+    		function Delete31()
+    		{
+    			document.getElementById("lblName31").innerHTML = ' ';
+    		}
+
+    		function Delete32()
+    		{
+    			document.getElementById("lblName32").innerHTML = ' ';
+    		}
+
+    		function Delete33()
+    		{
+    			document.getElementById("lblName33").innerHTML = ' ';
+    		}
+
+    		function Delete34()
+    		{
+    			document.getElementById("lblName34").innerHTML = ' ';
+    		}
+
+    		function Delete35()
+    		{
+    			document.getElementById("lblName35").innerHTML = ' ';
+    		}
+
+    		function Delete36()
+    		{
+    			document.getElementById("lblName36").innerHTML = ' ';
+    		}
+
+    		function Edit1()
+    		{
+    			var lblName = document.getElementById("lblName");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit2()
+    		{
+    			var lblName = document.getElementById("lblName2");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit3()
+    		{
+    			var lblName = document.getElementById("lblName3");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit4()
+    		{
+    			var lblName = document.getElementById("lblName4");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit5()
+    		{
+    			var lblName = document.getElementById("lblName5");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit6()
+    		{
+    			var lblName = document.getElementById("lblName6");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit7()
+    		{
+    			var lblName = document.getElementById("lblName7");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit8()
+    		{
+    			var lblName = document.getElementById("lblName8");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit9()
+    		{
+    			var lblName = document.getElementById("lblName9");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit10()
+    		{
+    			var lblName = document.getElementById("lblName10");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit11()
+    		{
+    			var lblName = document.getElementById("lblName11");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit12()
+    		{
+    			var lblName = document.getElementById("lblName12");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit13()
+    		{
+    			var lblName = document.getElementById("lblName13");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit14()
+    		{
+    			var lblName = document.getElementById("lblName14");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit15()
+    		{
+    			var lblName = document.getElementById("lblName15");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit16()
+    		{
+    			var lblName = document.getElementById("lblName16");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit17()
+    		{
+    			var lblName = document.getElementById("lblName17");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit18()
+    		{
+    			var lblName = document.getElementById("lblName18");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit19()
+    		{
+    			var lblName = document.getElementById("lblName19");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit20()
+    		{
+    			var lblName = document.getElementById("lblName20");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit21()
+    		{
+    			var lblName = document.getElementById("lblName21");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit22()
+    		{
+    			var lblName = document.getElementById("lblName22");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit23()
+    		{
+    			var lblName = document.getElementById("lblName23");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit24()
+    		{
+    			var lblName = document.getElementById("lblName24");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit25()
+    		{
+    			var lblName = document.getElementById("lblName25");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit26()
+    		{
+    			var lblName = document.getElementById("lblName26");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit27()
+    		{
+    			var lblName = document.getElementById("lblName27");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit28()
+    		{
+    			var lblName = document.getElementById("lblName28");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit29()
+    		{
+    			var lblName = document.getElementById("lblName29");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit30()
+    		{
+    			var lblName = document.getElementById("lblName30");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit31()
+    		{
+    			var lblName = document.getElementById("lblName31");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit32()
+    		{
+    			var lblName = document.getElementById("lblName32");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit33()
+    		{
+    			var lblName = document.getElementById("lblName33");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit34()
+    		{
+    			var lblName = document.getElementById("lblName34");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit35()
+    		{
+    			var lblName = document.getElementById("lblName35");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Edit36()
+    		{
+    			var lblName = document.getElementById("lblName36");
+    			var txtName = document.getElementById("txtName");
+
+	        	txtName.value = lblName.innerHTML;
+    		}
+
+    		function Highlight(x)
+    		{
+    			x.style.background="yellow";
+    		}
